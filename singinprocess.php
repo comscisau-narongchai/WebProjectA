e25719f3256fa9d2eb8f85b0586ee1c937b1bbe1
comscisau-narongchai- <s5752410037@sau.ac.th>
2017-03-07 16:10:47 +0700

    study DB 5752410037

---
+++ singinprocess.php
@@ -0,0 +1,36 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sing In Process</title>
+</head>
+<body>
+<?php
+
+	$uname = $_POST['uname'];
+	$upassword = $_POST['upassword'];
+
+
+	$host = "localhost";
+	$userdb = "root";
+	$passworddb = "";
+	$dbname = "comsci_db";
+	$conn = mysqli_connect($host,$userdb,$passworddb,$dbname);
+
+
+	$strsql = "select * from user_tb where uname='".$uname."' and upassword ='".$upassword."'";
+
+	$result = mysqli_query($conn, $strsql);
+
+	if($row = mysqli_fetch_array($result)){
+
+		echo "OK";
+	}else{
+		echo "User name และ Password ไม่ถูกต้อง";
+	}
+	
+		
+	
+?>
+</body>
+</html>
